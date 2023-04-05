ac5e78c4ccbc50ec149b813bbbddb69ae4abd3a3
punk9999 <punk9999@protonmail.com>
2023-04-04 20:40:06 -0400

    Add Mainnet scripts

---
+++ script/1MainnetDeploy.s.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.17;
+
+import "forge-std/Script.sol";
+import "forge-std/console2.sol";
+import "../src/NounScout.sol";
+import "../src/Interfaces.sol";
+
+/* Deploy NounScout */
+contract MainnetDeploy1 is Script {
+    NounScout nounScout;
+    INounsTokenLike nouns =
+        INounsTokenLike(0x9C8fF314C9Bc7F6e59A9d9225Fb22946427eDC03);
+    INounsAuctionHouseLike auctionHouse =
+        INounsAuctionHouseLike(0x830BD73E4184ceF73443C15111a1DF14e495C706);
+    IWETH weth = IWETH(0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2);
+
+    function setUp() public {}
+
+    function run() public {
+        vm.startBroadcast();
+        nounScout = new NounScout(nouns, auctionHouse, weth);
+
+        vm.stopBroadcast();
+        console2.log("nounScout", address(nounScout));
+    }
+}
---
+++ script/2MainnetDeploy.s.sol
@@ -0,0 +1,47 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.17;
+
+import "forge-std/Script.sol";
+import "forge-std/console2.sol";
+import "../src/NounScout.sol";
+
+/* Add Recipients */
+contract MainnetDeploy2 is Script {
+    NounScout nounScout = NounScout(0x30Dc2c9F7FC9aFEcd4f8146ba2461D81B7C1De5e);
+
+    function setUp() public {}
+
+    function run() public {
+        vm.startBroadcast();
+
+        // https://freedom.press/donate/cryptocurrency/
+        nounScout.addRecipient(
+            "Freedom Of The Press Foundation",
+            0x998F25Be40241CA5D8F5fCaF3591B5ED06EF3Be7,
+            "Freedom of the Press Foundation is a 501(c)(3) non-profit organization that protects, defends, and empowers public-interest journalism in the 21st century."
+        );
+
+        // https://archive.org/donate/cryptocurrency/
+        nounScout.addRecipient(
+            "Internet Archive",
+            0x1B40ed3d89fd40f875bF62A0ce79f562714d011E,
+            "The Internet Archive, a 501(c)(3) non-profit, is building a digital library of Internet sites and other cultural artifacts in digital form."
+        );
+
+        // https://rainforestfoundation.org/engage/give/cryptocurrency/
+        nounScout.addRecipient(
+            "Rainforest Foundation US",
+            0x338326660F32319E2B0Ad165fcF4a528c1994aCb,
+            "The mission of the Rainforest Foundation is to support indigenous and traditional peoples of the world's rainforests in their efforts to protect their environment and fulfill their rights."
+        );
+
+        // https://donate.torproject.org/cryptocurrency/
+        nounScout.addRecipient(
+            "Tor Project",
+            0x532Fb5D00f40ced99B16d1E295C77Cda2Eb1BB4F,
+            "The Tor Project, a 501(c)(3) US nonprofit, believes everyone should be able to explore the internet with privacy. We advance human rights and defend your privacy online through free software and open networks."
+        );
+
+        vm.stopBroadcast();
+    }
+}
