4931ca85f3f8c4a5eb8112a354fc4bbc71b200a3
punk9999 <punk9999@protonmail.com>
2023-04-30 20:38:24 -0400

    Solidity 0.8.17->0.8.19

--- src/NounScoutV2.sol
+++ src/NounScoutV2.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.17;
+pragma solidity ^0.8.19;
 
 import "./Interfaces.sol";
 import {Ownable2Step} from "openzeppelin-contracts/contracts/access/Ownable2Step.sol";
